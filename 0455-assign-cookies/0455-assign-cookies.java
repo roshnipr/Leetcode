9b739053463002f509ca46dccf7a9b5620a49e10
roshnipr <145689893+roshnipr@users.noreply.github.com>
2025-11-16 14:37:17 +0530

    Time: 11 ms (53.36%), Space: 48.6 MB (60.95%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        int i=0,j=0,count=0;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        while(i<g.length && j<s.length) {
+            if(g[i]<=s[j] ) {
+                i++;
+                j++;
+                count++;
+            } else {
+                j++;
+            }
+        }
+        return count;
+    }
+}
