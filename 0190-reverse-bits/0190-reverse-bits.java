5d96cb6c40c120e2c74980a2a4f0dc0921d7b58d
roshnipr <145689893+roshnipr@users.noreply.github.com>
2026-02-16 20:06:08 +0530

    Time: 0 ms (100%), Space: 42.3 MB (85.32%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int reverseBits(int n) {
+        int r=0;
+
+        for(int i=0;i<32;i++) {
+            r=r<<1;
+            r=r|(n&1);
+            n=n>>1;
+        }
+        return r;
+    }
+}
